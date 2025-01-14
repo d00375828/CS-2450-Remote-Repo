964a1bb05762d8348f661c5df962c0164f833159
Andy Rich <thegoat@MacBook-Air.local>
2025-01-14 15:33:45 -0700

    Third commit

--- guesser.py
+++ guesser.py
@@ -3,12 +3,12 @@ import random
 print('Hi I am going to guess your age!')
 name = input('What is your name?')
 
-#guesses = []
+guesses = []
 guessed = False
 while(guessed == False):
     guess = random.randint(15,30)
-    #if guess in guesses:
-       # continue
+    if guess in guesses:
+        continue
 
     user_response = input("Are you "+str(guess)+ " years old?")
     if user_response == 'y' or user_response =='Y':
@@ -17,4 +17,4 @@ while(guessed == False):
         break
     else:
         print('Rats')
-        #guesses.append(guess)
+        guesses.append(guess)
